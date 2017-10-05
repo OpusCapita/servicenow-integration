43ac2c7cf09d884b70565e18260290640eb396a7
StefanTuebben <stefan.tuebben@yahoo.de>
2017-10-05 23:35:03 +0200

    further refactoring

--- src/server/routes/refactored-insert.js
+++ src/server/routes/refactored-insert.js
@@ -14,30 +14,36 @@ module.exports = function (app, db, config) {
     app.get('/api/health-checks',
         (req, res) => doHealthCheck()
             .then((result) => res.send(result))
-            .catch((error) => res.status(500).send(error))
+            .catch((error) => res.status(500).send(error))  // TODO: escalation issue?
     );
 };
 
 let doHealthCheck = function () {
     return getServiceHealth()
-        .then(healthChecks => filterHealthChecks(healthChecks))
-        .then((filteredChecks) =>
+        .then(healthChecks => analyseHealthChecks(healthChecks))        // grouping by status and summing
+        .then(healthChecks => enrichWithDeploymentInfo(healthChecks))   // check for circle ci deployment
+        .then(healthChecks => enrichWithSevInfo(healthChecks))          // use enriched healthChecks to determine sev-state
+        .then(healthChecks => filterBySev(healthChecks))                // filtering based on sev
+        // TODO: check for duplicates on serviceNow
+        // TODO: how to get non-EVM-Ticket-ID ??!
+        // TODO: what if db is down and existing tickets cant be checked?
+        // .then(healthChecks => filterByExistingIssues(healthChecks))
+        .then(healthChecks =>
             Promise.all(
-                filteredChecks.map(
-                    serviceData => createHealthIssue(serviceData)
+                healthChecks.map(
+                    check => createHealthIssue(check)                   // creating issues
                 )
             )
-        )
+        );  // returns list of insert-responses(json)
 };
 
-let filterHealthChecks = function (healthChecks) {
+let analyseHealthChecks = function (healthChecks) {
     let filteredIssues = [];
     for (let currentService of healthChecks) {
         try {
             let serviceName = currentService[0].Service.Service;
             let totalChecks = 0;
             let passingChecks = 0;
-            let currentDeployments = checkDeployments(serviceName);
             for (let currentNode of currentService) {
                 const groupedChecks = helper.groupBy(currentNode.Checks, check => check.Status);
                 totalChecks += currentNode.Checks.length;
@@ -47,35 +53,54 @@ let filterHealthChecks = function (healthChecks) {
                 serviceName: serviceName,
                 total: totalChecks,
                 passed: passingChecks,
-                deploying: currentDeployments,
                 raw: currentService,
             };
-            serviceData['sev'] = 3; //getSevState(serviceData);
-            if (serviceData['sev'] > 0) {
-                filteredIssues.push(serviceData);
-            }
+            filteredIssues.push(serviceData);
         } catch (e) {
             log.error(e);
         }
     }
+    log.info(filteredIssues);
     return filteredIssues;
 };
 
-let checkDeployments = function (serviceName) {
-    // TODO: use circleCI API to check for current deployments
-    return false;
+let enrichWithDeploymentInfo = function (healthChecks) {
+    return Promise.all(
+        healthChecks.map(
+            check => {
+                check['deploying'] = 0; // TODO: use circle ci api
+                return check;
+            }
+        )
+    )
 };
 
 
-let createHealthIssue = function (serviceData) {
-    log.info("creating issue for service " + serviceData.serviceName);
+let enrichWithSevInfo = function (healthChecks) {
+    return Promise.all(
+        healthChecks.map(
+            check => {
+                check['sev'] = getSevState(check);
+                check['sev'] = 2;
+                return check;
+            }
+        )
+    )
+};
+
+let filterBySev = function (healthChecks) {
+    return healthChecks.filter(it => it['sev'] > 0);
+};
+
+
+let createHealthIssue = function (check) {
     let request = {
-        u_short_descr: createHealthIssueSubject(serviceData),
+        u_short_descr: createHealthIssueSubject(check),
         u_caller_id: 'TUBBEST1',    // Whos ocnet-id should be used?
         u_error_type: "\\OCSEFTP01\prod\Kundin\ssrca",	// List of error_types?
         //u_service: 'iPost Sweden',  // service needed?
-        u_priority: serviceData['sev'],
-        u_det_descr: createHealthIssueBody(serviceData),
+        u_priority: check['sev'],
+        u_det_descr: createHealthIssueBody(check),
         u_customer_id: 'OpusCapita' // TODO: what id are we using here?
     };
     return sendServiceNowRequest(request);
@@ -111,7 +136,12 @@ let getSevState = function (serviceData) {
 };
 
 let getServiceHealth = function () {
-    return config.consul.catalog.services()
+    return config.consul.catalog.services() // TODO: what if consul is dead? --> Escalation Ticket with what credentials?
+        .catch(error => {
+            log.error(error);
+            // TODO: create Escalation
+            return [];
+        })
         .then((services) => Object.keys(services))
         .then((serviceNames) =>
             Promise.all(
@@ -142,8 +172,39 @@ let doServiceNowInsert = function (client, request) {
             if (soapError) {
                 return reject(soapError)
             } else {
+                log.info(soapResponse);
                 return resolve(soapResponse)
             }
         });
     })
 };
+
+let createEscalationIssue = function (escalation) {
+    log.info("creating escalation issue!");
+    let request = {
+        u_short_descr: createEscalationIssueSubject(escalation),
+        u_caller_id: 'TUBBEST1',    // Whos ocnet-id should be used?
+        u_error_type: "\\OCSEFTP01\prod\Kundin\ssrca",	// List of error_types?
+        //u_service: 'iPost Sweden',  // service needed?
+        u_priority: 1,
+        u_det_descr: createEscalationIssueBody(escalation),
+        u_customer_id: 'OpusCapita' // TODO: what id are we using here?
+    };
+    createIssue(request);
+};
+
+let createEscalationIssueSubject = function (escalation) {
+    return helper.renderTemplate(`${__dirname}/templates/escalation_subject.njk`, escalation);
+};
+
+let createEscalationIssueBody = function (escalation) {
+    return helper.renderTemplate(`${__dirname}/templates/escalation_body.njk`, escalation);
+};
+
+class Escalation {
+    // TODO
+    constructor(reason, error) {
+        this.reason = reason;
+        this.error = error;
+    }
+}
