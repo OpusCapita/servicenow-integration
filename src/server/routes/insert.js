ab592e62c43e36e9582a669d8a7c0925eaec9535
Stefan Turbine <Stefan.Tuebben@yahoo.de>
2018-05-25 17:01:49 +0200

    - enhanced error response on wrong api use

--- src/server/routes/insert.js
+++ src/server/routes/insert.js
@@ -17,7 +17,7 @@ module.exports = function (app, db, config) {
         (req, res) => handleInsertByApi(req)
             .then(result => res.status(200).json(result))
             .catch(error => {
-                res.status(400).json(error);
+                res.status(400).json({message: error.message});
             })
     );
 };
