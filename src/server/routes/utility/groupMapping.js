6f4149788a729adca1ff2921362425500ce740bd
StefanTuebben <stefan.tuebben@yahoo.de>
2018-06-08 22:40:50 +0200

    [ci skip] added an additional assignment-group for the api

--- src/server/routes/utility/groupMapping.js
+++ src/server/routes/utility/groupMapping.js
@@ -1,4 +1,5 @@
 module.exports = {
     'plattform': 'OC CS GLOB Service Desk AM',
-    'bnp_support': 'Business Network Messaging Platform Monitoring'
+    'bnp_support': 'Business Network Messaging Platform Monitoring',
+    'bnp_devops': 'Business Network Portal DevOps'
 };
