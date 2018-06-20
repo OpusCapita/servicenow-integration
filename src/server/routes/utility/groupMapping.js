c6fe35b13b644ac2de6c1937ddfa383c31d2ed08
StefanTuebben <stefan.tuebben@yahoo.de>
2018-06-20 21:07:22 +0200

    deploy.me

--- src/server/routes/utility/groupMapping.js
+++ src/server/routes/utility/groupMapping.js
@@ -1,5 +1,5 @@
 module.exports = {
     'plattform': 'OC CS GLOB Service Desk AM',
     'bnp_support': 'Business Network Messaging Platform Monitoring',
-    'bnp_devops': 'Business Network Portal DevOps'
+    'bnp_devops': 'Business Network Portal DevOps' 
 };
