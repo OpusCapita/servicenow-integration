d9e9ece3a16185036fb76a687e9ca631c827e25f
Stefan Turbine <Stefan.Tuebben@yahoo.de>
2018-06-25 12:05:26 +0200

    deploy.me

--- src/server/routes/utility/groupMapping.js
+++ src/server/routes/utility/groupMapping.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'plattform': 'Business Network Portal DevOps',
+    'plattform': 'Business Network Portal DevOps', 
     'bnp_support': 'Business Network Messaging Platform Monitoring',
     'bnp_devops': 'Business Network Portal DevOps' 
 };
