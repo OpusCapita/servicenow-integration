0b733e05860dcc1cf7bd55831b94185fa1e1728c
Stefan Turbine <Stefan.Tuebben@yahoo.de>
2018-06-25 11:56:25 +0200

    changed assignment group

--- src/server/routes/utility/groupMapping.js
+++ src/server/routes/utility/groupMapping.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'plattform': 'OC CS GLOB Service Desk AM',
+    'plattform': 'Business Network Portal DevOps',
     'bnp_support': 'Business Network Messaging Platform Monitoring',
     'bnp_devops': 'Business Network Portal DevOps' 
 };
